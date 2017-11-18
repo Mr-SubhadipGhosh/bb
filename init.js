a393973f96de0b4ce1c196a8342186da77b2fe26
unknown <ghosh_su@Subhadip-G.India.XoriantCorp.com>
2017-11-18 12:25:50 +0530

    updated

--- init.js
+++ init.js
@@ -4,9 +4,8 @@ var jpp = require('json-path-processor');
 var fs = require('fs');
 var _res_flag, _res,_tmp, _indx;
 function node(data){
-    this.d=data;
-    this.p=[];
-    this.n=[];
+    this.v=data;
+    this.d=[];
 }
 function init(input){  
   get(function (br) {
@@ -16,12 +15,12 @@ function init(input){
     if(!_res_flag){           
       if(i==0){     
          y = new node(input[i]);
-         br.n.push(y);
+         br.d.push(y);
       }
       else{
         var x=new node(input[i]);
         console.log(y);
-        y.n.push(x);
+        y.d.push(x);
         y=x;
       }
     }
@@ -32,7 +31,10 @@ function init(input){
       else{       
         console.log(y.n);
         var tmp= jpp(br).get(_res);
-        y.n.push(tmp._data);
+        var isThr = y.d.filter(x=>x===tmp._data);
+        if(!(isThr.length>0)){
+         y.d.push(tmp._data);
+        }
         y=tmp._data;
       }
     }   
@@ -43,7 +45,7 @@ function init(input){
 function findinit(input, br){  
   _res_flag = false;
   for(let {node, path} of new Rector(br,0,true)) {
-    if(path[path.length-1]=='d' && node==input){
+    if(path[path.length-1]=='v' && node==input){
       _res_flag=true;
       path.splice(-1,1);
      _res= path.join('.');
@@ -65,4 +67,4 @@ fs.readFile('foo.txt', 'utf8', function (err,data) {
   cb(CircularJSON.parse(data));
 });
 }
-init("i love a ball");
+init("1+2=3");
